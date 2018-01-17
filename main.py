e7c56ff309e2617dc057d2b2668e99a5173721e0
Gulcin Ennis <gulcin.ennis@gmail.com>
2018-01-16 20:01:57 -0500

    final commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,33 +18,37 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
+
 @app.route('/')
 def index():
     return redirect('/blog')
+
 
 @app.route('/blog')
 def blog():
     blogs = Blog.query.all()
     return render_template('blog.html', blogs=blogs)
 
+
+@app.route('/singleblog')
 def single_blog():
     id = int(request.args.get('id'))
-    blog = Blog.query.filter_by(blog_id)
+    blog = Blog.query.filter_by(id=id).first()
     return render_template('single_blog.html', id=id, blog=blog)
     
-    
+     
 @app.route('/newpost', methods=['GET', 'POST'])
 def new_post():
     if request.method == 'GET':
         return render_template('new_post.html')
 
     if request.method == 'POST':
-       
         blog_title = request.form['blog_title']
         blog_body = request.form['blog_body']
        
         title_error = ''
-        body_error = ''
+        blog_body_error = ''
+
 
         if blog_title == "":
             title_error = "Please enter a title for your blog"
@@ -52,11 +56,13 @@ def new_post():
         if blog_body == "":
             blog_body_error = "Please enter content for your blog"
 
-        if not blog_body_error or title_error:   
-            new_blog = Blog(blog_title, blog_body)
-            db.session.add(new_blog)
+        if blog_body_error or title_error:
+            return render_template('new_post.html', blog_title=blog_title, blog_body=blog_body, title_error=title_error, blog_body_error=blog_body_error)
+        else: 
+            blog = Blog(blog_title, blog_body)
+            db.session.add(blog)
             db.session.commit()
-            return render_template('single_blog.html', blog=blog)    
+            return render_template('single_blog.html', blog=blog, blog_title=blog_title, blog_body=blog_body)    
 
 
 if __name__ == '__main__':
